854edeb033327796d98f66e07adffcabdf320292
FedorKh <31988354+FedorKh@users.noreply.github.com>
2017-09-18 22:58:36 +0300

    Update submenu.js

--- Ambulance_17_09/jQuery/submenu.js
+++ Ambulance_17_09/jQuery/submenu.js
@@ -2,23 +2,29 @@
 (function($){
     $(document).ready(function(){
 
-        var w=window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-        var h=window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+        //var w=window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        //var h=window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
 
         $(".main-list").click(function(){
 
             $(document).ready(function(){
 
+                        var w=window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
                         var leftPositionParent = $(this).find(".sublist:visible").offset().left;
                         var widthParent = $(this).find(".sublist:visible").width();
 
                         if((leftPositionParent + widthParent) > w){
                             var delta = w - (leftPositionParent + widthParent);
                             $(this).find(".sublist:visible").offset({left: leftPositionParent + delta-10});
-                            arr.unshift(leftPositionParent);
                         }
-
+                        else{
+                            $(this).find(".sublist:visible").css("left","0px");
+			                var deltaNew = w - ($(this).find(".sublist:visible").offset().left+$(this).find(".sublist:visible").width());
+			                    if(deltaNew < 0){
+				                    $(this).find(".sublist:visible").offset({left:  $(this).find(".sublist:visible").offset().left + deltaNew-10});
+			                    }
+                    	}
             });
 
         });
@@ -33,6 +39,7 @@
 
             $(document).ready(function(){
 
+                var w=window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
                 var leftPosition = $(this).find(".subsublist:visible").offset().left;
                 var widthElement = $(this).find(".subsublist:visible").width();
                 var leftPositionParent = $(this).find(".subsublist:visible").parent().parent().offset().left;
@@ -47,15 +54,21 @@
                         $(this).find(".subsublist:visible").offset({left: 200});
                     }
                 }
+                else{
+                    $(this).find(".subsublist:visible").css("left","100%");
+                    var deltaNew = w - ($(this).find(".subsublist:visible").offset().left+$(this).find(".subsublist:visible").width());
+                    if(deltaNew<0){
+                        $(this).find(".subsublist:visible").parent().parent().offset(
+                            {left: $(this).find(".subsublist:visible").parent().parent().offset().left + deltaNew-3});
+                        $(this).find(".subsublist:visible").offset(
+                            {left: $(this).find(".subsublist:visible").offset().left + deltaNew-3});
+                        if($(this).find(".subsublist:visible").parent().parent().offset().left < 0){
+                            $(this).find(".subsublist:visible").parent().parent().offset({left: 0});
+                            $(this).find(".subsublist:visible").offset({left: 200});
+                        }
+                    }
+                }
             });
-
         });
-
-       /*$(window).resize(function(){
-           var changeWidth;
-           if((changeWidth>w)||((changeWidth<w)){
-                   location.reload();
-           }
-        });*/
     });
 })(jQuery);
